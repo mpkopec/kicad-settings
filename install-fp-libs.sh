be322241b52d79d12b131a2e58e94c9d7718198d
Maciej Kopeć <maciejpkopec@gmail.com>
2016-08-20 21:37:28 +0200

    Installation script with nice formatting and progress

--- install-fp-libs.sh
+++ install-fp-libs.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 last_dir=`pwd`
+green='\033[1;32m'
+nc='\033[0m'
 
 if [[ ! -d $1 ]]; then
 	echo "Please provide a directory to which .pretty libraries should be"
@@ -9,6 +11,18 @@ if [[ ! -d $1 ]]; then
 fi
 cd $1
 
-curl 'https://api.github.com/users/KiCad/repos?per_page=10000' | jq '.[] | select(.name | test(".pretty")) | .clone_url' | xargs -I url git clone url
+repos=`curl 'https://api.github.com/users/KiCad/repos?per_page=10000' | jq '.[] | select(.name | test(".pretty")) | .name, .clone_url'`
+readarray -t reposArray <<<"$repos"
+i=0
+n=${#reposArray[@]}
+while [ "$i" -lt "$n" ]; do
+	reposArray[$i]=`echo "${reposArray[$i]}" | tr -d '"'`
+	printf "Cloning ${green}${reposArray[$i]}${nc} (repository ${green}%d/%d${nc})\n" "$(((i+2)/2))" "$((n/2))"
+	let i++
+
+	reposArray[$i]=`echo "${reposArray[$i]}" | tr -d '"'`
+	git clone ${reposArray[$i]}
+	let i++
+done
 
 cd $last_dir
